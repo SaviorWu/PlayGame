d2d4eedb09ac7fc4b93b1897455b842780db350e
FriendWu <407339176@qq.com>
2020-11-26 01:20:58 +0800

    内购充值测通

--- PlayGame/Business/My/Business/MyPorketVC/Business/RechargeVC.m
+++ PlayGame/Business/My/Business/MyPorketVC/Business/RechargeVC.m
@@ -25,6 +25,7 @@
     self.selectIdx = -1;
     self.selectAppleGoodsID = @"";
     [self addNavigationView];
+    [[SKPaymentQueue defaultQueue] addTransactionObserver:self];
 }
 
 - (void)loadUI{
@@ -140,45 +141,56 @@
 }
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
+    
     if (indexPath.row == 2) {
         self.selectIdx = 1;
         self.money = @"10";
-        self.selectAppleGoodsID = @"";
+        self.selectAppleGoodsID = @"cszs8ansfpsfelofgzek7ga3bb3e0icj";
     }else if (indexPath.row == 4) {
         self.selectIdx = 2;
         self.money = @"30";
-        self.selectAppleGoodsID = @"";
+        self.selectAppleGoodsID = @"cfhhnw0199gt8quam43i2qpli6whobvu";
     }else if (indexPath.row == 6) {
         self.selectIdx = 3;
         self.money = @"60";
-        self.selectAppleGoodsID = @"";
+        self.selectAppleGoodsID = @"x77c4ate05yzbt6f96yd8zrnxhyxl6y8";
     }else if (indexPath.row == 8) {
         self.selectIdx = 4;
         self.money = @"200";
-        self.selectAppleGoodsID = @"";
+        self.selectAppleGoodsID = @"cnw26qwdnd1qunkukzgemq3s97f2iuve";
     }else if (indexPath.row == 10) {
         self.selectIdx = 5;
         self.money = @"500";
-        self.selectAppleGoodsID = @"";
+        self.selectAppleGoodsID = @"3h693zvxyn7j0054cqz9fnqgnsjmx8ti";
     }else if (indexPath.row == 12) {
         self.selectIdx = 6;
         self.money = @"1000";
-        self.selectAppleGoodsID = @"";
+        self.selectAppleGoodsID = @"8obc7c9srmlct8atc46j0sb8m5n8jia5";
     }
     [self loadUI];
 }
 
 
-
+- (void)viewDidDisappear:(BOOL)animated
+{
+    [super viewDidDisappear:animated];
+    [[SKPaymentQueue defaultQueue] removeTransactionObserver:self];
+}
 - (void)requestServerOrderInfo {
     self.view.userInteractionEnabled = NO;
-    [self showHudInView:self.view];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [self showHudInView:self.view];
+    });
+    
     [JTNetwork requestPostWithParam:@{@"ys":[UserModelManager shareInstance].userModel.token,
                                       @"sb":@"ios",
                                       @"sp":self.selectAppleGoodsID,
                                       @"rmb":self.money
     } url:@"/ping/mei/czd" callback:^(JTBaseReqModel *model) {
-        [self hideAllHud];
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self hideAllHud];
+        });
         self.productId = model.sj;
         [self orderToApplePay];
     }];
@@ -193,7 +205,10 @@
     if ([SKPaymentQueue canMakePayments]) {
         NSLog(@"用户允许内购");
         
-        [self showHudInView:self.view];
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self showHudInView:self.view];
+        });
         NSArray *product = [[NSArray alloc] initWithObjects:self.selectAppleGoodsID,nil];
         NSSet *nsset = [NSSet setWithArray:product];
         
@@ -217,7 +232,10 @@
     //如果服务器没有产品
     if([product count] == 0){
         [self showHint:@"没有该商品"];
-        [self hideAllHudFromSuperView:self.view];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self hideAllHudFromSuperView:self.view];
+        });
+        
         return;
     }
     
@@ -238,14 +256,16 @@
     
     //发送购买请求
     SKMutablePayment *payment = [SKMutablePayment paymentWithProduct:requestProduct];
-    payment.applicationUsername = [NSString stringWithFormat:@"%@%@",[UserModelManager shareInstance].userModel.uid,self.productId];
+    payment.applicationUsername = [NSString stringWithFormat:@"%@%@_1",[UserModelManager shareInstance].userModel.uid,self.productId];
     [[SKPaymentQueue defaultQueue] addPayment:payment];
 }
 #pragma mark - SKRequestDelegate
 //请求失败
 - (void)request:(SKRequest *)request didFailWithError:(NSError *)error
 {
-    [self hideAllHudFromSuperView:self.view];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [self hideAllHudFromSuperView:self.view];
+    });
     NSLog(@"error:%@", error);
 }
 
@@ -264,8 +284,11 @@
             case SKPaymentTransactionStatePurchased:
             {
                 NSLog(@"交易完成");
-                [self hideAllHudFromSuperView:self.view];
-                [self showHudInView:self.view];
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    [self hideAllHudFromSuperView:self.view];
+                    [self showHudInView:self.view];
+                });
+                
                 [[SKPaymentQueue defaultQueue] finishTransaction:tran];
                 [self completeTransaction:tran];
             }
@@ -280,7 +303,10 @@
             {
                 NSLog(@"交易失败");
                 self.view.userInteractionEnabled = YES;
-                [self hideAllHudFromSuperView:self.view];
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    [self hideAllHudFromSuperView:self.view];
+                });
+                
                 [self showHint:@"购买失败" delay:1.3];
                 [[SKPaymentQueue defaultQueue] finishTransaction:tran];
                 
@@ -313,7 +339,10 @@
     NSData *result = [NSURLConnection sendSynchronousRequest:urlRequest returningResponse:nil error:nil];
     if (result == nil) {
         NSLog(@"验证失败");
-        [self hideAllHudFromSuperView:self.view];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self hideAllHudFromSuperView:self.view];
+        });
+        
         return;
     }
     NSDictionary *dic = [NSJSONSerialization JSONObjectWithData:result options:NSJSONReadingAllowFragments error:nil];
@@ -331,7 +360,10 @@
         [JTNetwork requestGetWithParam:@{@"ys":[UserModelManager shareInstance].userModel.token,
                                          @"receipt":self.receipt
         } url:@"/ping/mei/cz" callback:^(JTBaseReqModel *model) {
-            [self hideAllHud];
+            
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self hideAllHud];
+            });
             [self PaySuccess];
             [[SKPaymentQueue defaultQueue] finishTransaction: transaction];
         }];
